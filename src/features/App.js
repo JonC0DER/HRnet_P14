39b786f47b058da696dd0abb39d2ab59754ae616
joncoder <joncoder.kamite@gmail.com>
2023-01-16 09:10:57 +0100

    creation de la feature App

---
+++ src/features/App.js
@@ -0,0 +1,13 @@
+import Head from '../components/Head'
+import { RouterLogic } from '../components/RouterLogic'
+
+const App = () => {
+    return (
+        <>
+            <Head />
+            <RouterLogic />
+        </>
+    )
+}
+
+export default App
