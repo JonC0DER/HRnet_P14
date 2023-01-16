c90099038bfc7af6147ae9dac8bcc0deed477eeb
joncoder <joncoder.kamite@gmail.com>
2023-01-16 09:13:33 +0100

    creation du composant header

---
+++ src/components/Head.js
@@ -0,0 +1,12 @@
+
+const Head = () => {
+    return (
+        <header>
+            <div className="title">
+                <h1>HRnet</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Head
