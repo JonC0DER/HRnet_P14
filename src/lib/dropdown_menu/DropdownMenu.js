71b61f7d4b4e0d8cb5a4bd49e11e762fe50e18f8
joncoder <joncoder.kamite@gmail.com>
2023-01-16 09:06:03 +0100

    creation du plugin dropdown menu

---
+++ src/lib/dropdown_menu/DropdownMenu.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+const DropdownMenu = ({ datas }) => {
+
+    return (
+        <>
+            <option value="">Choose your country</option>
+            {datas.map((elem, id) =>
+                <option
+                    key={id}
+                    value={elem.abbreviation}
+                >
+                    {elem.name}
+                </option>
+            )}
+        </>
+    )
+}
+
+DropdownMenu.propTypes = {
+    datas: PropTypes.arrayOf(
+        PropTypes.objectOf(PropTypes.string)
+    )
+}
+
+export default DropdownMenu
