41815d48252bd63db75be1faa7b74e4fc82c1a97
joncoder <joncoder.kamite@gmail.com>
2023-01-22 00:35:58 +0100

    renommage du composant NewEmployeeContainer -> Home

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+import Form from "../components/Form"
+
+const NewEmployeeContainer = () => {
+    return (
+        <div className="container">
+            <NavLink to="/employee-list">View current employees</NavLink>
+            <Form />
+        </div>
+    )
+}
+
+export default NewEmployeeContainer
