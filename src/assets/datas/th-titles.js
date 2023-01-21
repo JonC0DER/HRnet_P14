83f9782431916f6c98840bee6a262b6c4d9f5056
joncoder <joncoder.kamite@gmail.com>
2023-01-22 00:27:04 +0100

    creation d'un JSON pour envoyer les titres th du tableau au composant

---
+++ src/assets/datas/th-titles.js
@@ -0,0 +1,13 @@
+const thTitles = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    birthDate: "Birth of Date",
+    startDate: "Start Date",
+    street: "Street",
+    city: "City",
+    countryState: "State",
+    zipCode: "Zip Code",
+    department: "Department"
+}
+
+export default thTitles
