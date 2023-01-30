aa481324a7f28f04993333626b786ff6c143e652
joncoder <joncoder.kamite@gmail.com>
2023-01-30 14:17:23 +0100

    add reset route path

--- src/components/RouterLogic.js
+++ src/components/RouterLogic.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ResetModaleMsg } from "../features/resetModaleMsg"
 import Home from '../pages/Home'
 import ListEmployees from "../pages/ListEmployees"
 
@@ -9,6 +10,7 @@ export const RouterLogic = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/employee-list" element={<ListEmployees />} />
+                <Route path="/reset-employee" element={<ResetModaleMsg />} />
             </Routes>
         </BrowserRouter>
     )
